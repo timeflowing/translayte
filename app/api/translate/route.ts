3b36c78ef3b5ede6a6a82c7578e9be820d12747d
Vojta Tomasek <fargy33@gmail.com>
2025-07-01 01:38:41 +0200

    s

--- app/api/translate/route.ts
+++ app/api/translate/route.ts
@@ -7,9 +7,9 @@ import { getApps, initializeApp } from 'firebase-admin/app';
 if (!getApps().length) {
   initializeApp({
     credential: makeCert({
-      projectId: process.env.FIREBASE_PROJECT_ID!,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL!,
+      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
     }),
   });
 }
--- app/lib/firebaseAdmin.ts
+++ app/lib/firebaseAdmin.ts
@@ -3,15 +3,15 @@ import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
 const sa: ServiceAccount = {
-  projectId:    process.env.FIREBASE_PROJECT_ID!,
-  clientEmail:  process.env.FIREBASE_CLIENT_EMAIL!,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  projectId:    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  clientEmail:  process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL!,
+  privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
 
 if (!getApps().length) {
   initializeApp({
     credential: cert(sa),
-    // databaseURL: process.env._FIREBASE_DATABASE_URL,  // if using RTDB
+    // databaseURL: process.env._NEXT_PUBLIC_FIREBASE_DATABASE_URL,  // if using RTDB
   });
 }
 
