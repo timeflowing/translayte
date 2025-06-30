c8735c3a71798ca5aa84553b3d3773cc71361117
Vojta Tomasek <fargy33@gmail.com>
2025-07-01 01:19:40 +0200

    s

--- app/lib/firebaseAdmin.ts
+++ app/lib/firebaseAdmin.ts
@@ -5,7 +5,7 @@ import { getFirestore } from 'firebase-admin/firestore';
 const sa: ServiceAccount = {
   projectId:    process.env.FIREBASE_PROJECT_ID!,
   clientEmail:  process.env.FIREBASE_CLIENT_EMAIL!,
-  privateKey:   process.env.FIREBASE_PRIVATE_KEY!,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
 
 if (!getApps().length) {
